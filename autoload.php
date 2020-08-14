ce77d044943e8fc5ce5d7075d77ee5a64251066a
Akalabaya <65589371+Akalabaya@users.noreply.github.com>
2020-08-14 11:38:58 -0400

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
-function convertapi($from,$to,$url){
-header('location:api.php?from='.$from."&to=".$to."&url=".$url);
+function convertapi($from,$to,$url,$reditecturi){
+header('location:api.php?from='.$from."&to=".$to."&url=".$url."&redirecturi=".$redirecturi);
 }
 
 ?>
